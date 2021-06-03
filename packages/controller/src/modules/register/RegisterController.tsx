7722868619247b13620401ff8f39d67c02635d8c
Dolosolow <Jose.Munoz07c@gmail.com>
2021-06-03 06:38:05 -0400

    Fixed Invalid hook call error. Updated deps, fix was a change to app package; file 'meto.config.js'.

---
+++ packages/controller/src/modules/register/RegisterController.tsx
@@ -0,0 +1,34 @@
+import { gql } from "@apollo/client";
+import { useRegisterMutation, MutationRegisterArgs } from "../../types/graphql";
+
+interface RCProps {
+  children: (data: {
+    loading: boolean;
+    submit: (values: MutationRegisterArgs) => Promise<null>;
+  }) => JSX.Element;
+}
+
+export function RegisterController(props: RCProps) {
+  const [newRegistration, { loading }] = useRegisterMutation();
+
+  const onSubmit = async (values: MutationRegisterArgs) => {
+    try {
+      await newRegistration({ variables: values });
+      return null;
+    } catch (err) {
+      console.log("error: ", err);
+      return err;
+    }
+  };
+
+  return props.children({ loading, submit: onSubmit });
+}
+
+export const RegisterMutation = gql`
+  mutation Register($email: String!, $password: String!) {
+    register(email: $email, password: $password) {
+      path
+      message
+    }
+  }
+`;
